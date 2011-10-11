205f1f515c113334343a7d7bf51c086560efe479
Dan Newcome <djn125@yahoo.com>
2011-10-11 03:23:27 +0000

    createdb script now prompts for password - I don't want to have pw in clear text in any scripts

--- createdb.sh
+++ createdb.sh
@@ -4,13 +4,13 @@
 ## Warning: current db will be dropped
 
 ## configure db name and auth
-dbname=workingon
+dbname=newsjs
 dbuser=root
 dbpass=root
 
 echo ${dbname}
 
-mysql -u $dbuser --password=$dbpass -e "drop database $dbname;"
-mysql -u $dbuser --password=$dbpass -e "create database $dbname default character set utf8;"
+mysql -u $dbuser -p -e "drop database $dbname;"
+mysql -u $dbuser -p -e "create database $dbname default character set utf8;"
 
-mysql -u $dbuser --password=root $dbname < schema.sql
+mysql -u $dbuser -p $dbname < schema.sql
