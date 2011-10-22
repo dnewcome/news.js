6d06d55f573b04512634d8e333df8243838ce676
Dan Newcome <dnewcome@elevenprime.com>
2011-10-22 02:08:28 -0700

    updated schema for 40char pw hash

--- schema.sql
+++ schema.sql
@@ -12,7 +12,8 @@ create table project (
 create table user (
 	id integer not null auto_increment primary key, 
 	username varchar(25), 
-	password varchar(25),
+	-- pw hashed using sha1, needs 40 hex chars
+	password varchar(40),
 	unique( username )
 );
 
